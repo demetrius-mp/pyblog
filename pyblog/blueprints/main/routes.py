ed6f577c565b39f87ea5700654b1bd52bd5a51dd
Demetrius Panovitch <demetrius.mp789@gmail.com>
2021-09-24 00:51:03 -0400

    feat: search in sidebar

--- pyblog/blueprints/main/routes.py
+++ pyblog/blueprints/main/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, Blueprint, request, redirect
 
+from pyblog.blueprints.main.forms import SearchForm
 from pyblog.blueprints.user.forms import RegistrationForm, LoginForm
+from pyblog.extensions import auth
 from pyblog.models import Post
 
 main = Blueprint('main', __name__)
@@ -12,16 +14,33 @@ def index():
     if next_page:
         return redirect(next_page)
 
-    registration_form = RegistrationForm()
-    login_form = LoginForm()
+    search_query = request.args.get('search')
+    search_form = SearchForm()
 
-    posts: list[Post] = Post.query.filter_by(is_published=True).all()
+    if search_query:
+        search_form.search.data = search_query
+        search_query = f'%{search_query}%'
+        # noinspection PyUnresolvedReferences
+        posts: list[Post] = Post.query.filter(Post.title.like(search_query)).all()
+
+    else:
+        posts: list[Post] = Post.query.filter_by(is_published=True).all()
+
     recommended_posts: list[Post] = (
         Post.query.filter_by(is_published=True)
             .order_by(Post.id).limit(3).all()
     )
 
+    if auth.current_user.is_authenticated:
+        return render_template('index.html', title='Home', posts=posts,
+                               recommended_posts=recommended_posts,
+                               search_form=search_form)
+
+    registration_form = RegistrationForm()
+    login_form = LoginForm()
+
     return render_template('index.html', title='Home',
                            registration_form=registration_form,
                            login_form=login_form, posts=posts,
-                           recommended_posts=recommended_posts)
+                           recommended_posts=recommended_posts,
+                           search_form=search_form)
