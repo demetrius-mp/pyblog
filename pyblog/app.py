52909b7bdaea0ad3929f72bbbe4c8d8cfaacd536
Demetrius Panovitch <demetrius.mp789@gmail.com>
2021-09-23 22:32:57 -0400

    config: add .secrets.toml

--- pyblog/app.py
+++ pyblog/app.py
@@ -6,6 +6,5 @@ from pyblog.extensions import configuration
 def create_app():
     app = Flask(__name__)
     configuration.init_app(app)
-    configuration.load_extensions(app)
 
     return app
--- pyblog/extensions/configuration.py
+++ pyblog/extensions/configuration.py
@@ -1,17 +1,8 @@
-from importlib import import_module
-
 from dynaconf import FlaskDynaconf
 
-
-def load_extensions(app):
-    for extension in app.config.EXTENSIONS:
-        # Split data in form `extension.path:factory_function`
-        module_name, factory = extension.split(":")
-        # Dynamically import extension module.
-        ext = import_module(module_name)
-        # Invoke factory passing app.
-        getattr(ext, factory)(app)
+flask_dynaconf = FlaskDynaconf()
 
 
 def init_app(app, **config):
-    FlaskDynaconf(app, **config)
+    flask_dynaconf.init_app(app, **config)
+    app.config.load_extensions()
