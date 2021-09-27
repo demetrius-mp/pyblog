0430e260a55d7b5f2c8f0bf98e7aa459c5dc34c5
Demetrius Panovitch <demetrius.mp789@gmail.com>
2021-09-26 22:32:32 -0400

    trying to support heroku

--- pyblog/app.py
+++ pyblog/app.py
@@ -1,7 +1,3 @@
-import os
-import random
-import string
-
 from flask import Flask
 
 from pyblog.extensions import configuration
@@ -9,13 +5,6 @@ from pyblog.extensions import configuration
 
 def create_app():
     app = Flask(__name__)
-
-    env_vars = ('SECRET_KEY',)
-
-    for env_var in env_vars:
-        if env_var not in app.config:
-            random_secret_key = ''.join(random.choices(string.ascii_uppercase + string.digits, k=32))
-            app.config[env_var] = os.environ.get(env_var, random_secret_key)
     configuration.init_app(app)
 
     return app
