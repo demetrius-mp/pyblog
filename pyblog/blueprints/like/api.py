67643f2b33986a92859fe6f3df350f21c499f1db
Demetrius Panovitch <demetrius.mp789@gmail.com>
2021-09-30 10:39:59 -0400

    feat: decorators for post not found and anonymous user

--- pyblog/blueprints/like/api.py
+++ pyblog/blueprints/like/api.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from flask import Blueprint, jsonify
 from sqlalchemy.exc import IntegrityError
 
@@ -8,24 +10,37 @@ from pyblog.models import Like, Post
 api = Blueprint('likes_api', __name__, url_prefix='/api/likes')
 
 
-# noinspection DuplicatedCode
+def post_must_exist(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        post = Post.query.get(kwargs['post_id'])
+        if not post:
+            return jsonify({
+                'msg': 'Post not found.',
+                'category': 'info'
+            }), 404
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def login_required_api(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        current_user = auth.current_user
+        if not current_user.is_authenticated:
+            return jsonify({
+                'msg': 'You must login to like a post.',
+                'category': 'info'
+            }), 401
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 @api.post('/<int:post_id>')
+@post_must_exist
+@login_required_api
 def like_post(post_id: int):
-    current_user = auth.current_user
-    if not current_user.is_authenticated:
-        return jsonify({
-            'msg': 'You must login to like a post.',
-            'category': 'info'
-        }), 401
-
-    post = Post.query.get(post_id)
-    if not post:
-        return jsonify({
-            'msg': 'Post not found.',
-            'category': 'info'
-        }), 404
-
-    like = Like(user_id=current_user.id, post_id=post.id)
+    like = Like(user_id=auth.current_user.id, post_id=post_id)
     session = get_session()
     session.add(like)
 
@@ -43,8 +58,9 @@ def like_post(post_id: int):
     })
 
 
-# noinspection DuplicatedCode
 @api.delete('/<int:post_id>')
+@post_must_exist
+@login_required_api
 def dislike_post(post_id: int):
     current_user = auth.current_user
     if not current_user.is_authenticated:
